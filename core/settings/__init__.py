1b5694ac4eaaa84cd7f0b64d5b35c71f55929998
john <john@cupist.com>
2019-07-26 18:17:52 +0900

    Make app frame
    1. Implement app factory
    2. Make config
    3. Make apps frame

---
+++ core/settings/__init__.py
@@ -0,0 +1,40 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Config:
+    env: str
+    debug: bool = os.getenv('DEBUG', True)
+    app_host: str = os.getenv('APP_HOST', '0.0.0.0')
+    app_port: int = os.getenv('APP_PORT', 8000)
+    db_host: str = os.getenv('DB_HOST', '')
+    db_port: int = os.getenv('DB_PORT', 3306)
+
+
+@dataclass(frozen=True)
+class DevelopmentConfig(Config):
+    env: str = 'development'
+    debug: bool = True
+
+
+@dataclass(frozen=True)
+class TestingConfig(Config):
+    env: str = 'testing'
+    debug: bool = True
+
+
+@dataclass(frozen=True)
+class ProductionConfig(Config):
+    env: str = 'production'
+    debug: bool = False
+
+
+def get_config():
+    env = os.getenv('env', 'development')
+    config_type = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig
+    }
+    return config_type[env]
