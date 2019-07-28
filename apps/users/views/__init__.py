44e83e43c30df89b4089612dc54eb78c5cda3cd6
John <john@cupist.com>
2019-07-28 19:02:01 +0900

    Register User class to blueprint

--- apps/users/views/__init__.py
+++ apps/users/views/__init__.py
@@ -1,6 +1,7 @@
 from sanic import Blueprint
-from apps.users.views.v1 import UserList
+from apps.users.views.v1 import UserList, User
 
 
 bp = Blueprint('users', url_prefix='/api/v1')
 bp.add_route(UserList.as_view(), '/users')
+bp.add_route(User.as_view(), '/users/<user_id:int>')
